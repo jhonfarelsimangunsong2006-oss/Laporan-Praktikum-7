e90a3eaf2e50ea480ada19930b33f38fca5440dc
jhonfarelsimangunsong2006-oss <jhonfarelsimangunsong2006@mail.ugm.ac.id>
2025-11-02 21:20:19 +0700

    Update Latihan 1.cpp

--- Latihan 1.cpp
+++ Latihan 1.cpp
@@ -16,12 +16,13 @@ int main()
     for(i=1 ; i<5 ;i++){
     cout<<"masukkan nilai celcius "<<i<<":";
     cin>>x[i];}
-    cout<<left<<setw(10)<<"celcius"<<right<<setw(17)<<"fahrenheit"<<right<<setw(18)<<"kelvin"<<endl;
+    cout<<right<<setw(10)<<"celcius"<<right<<setw(17)<<"fahrenheit"<<right<<setw(18)<<"kelvin"<<endl;
     cout<<"----------------------------------------------------"<<endl;
     for(i=1 ; i<5 ; i++){
-    cout<<left<<setw(10)<<x[i]<<right<<setw(17)<<suhu1(x[i])<<right<<setw(18)<<suhu2(x[i])<<endl;
+    cout<<right<<setw(10)<<x[i]<<right<<setw(17)<<suhu1(x[i])<<right<<setw(18)<<suhu2(x[i])<<endl;
 }
 
 return 0;
 }
+
 
